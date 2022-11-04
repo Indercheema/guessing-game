7b510fc1906ce981ca626e42e239ad8d3a9ec1ba
inder <cheema.inder10@gmail.com>
2022-11-04 12:27:30 -0500

    bugs fixed

--- assets/script/index.js
+++ assets/script/index.js
@@ -44,8 +44,8 @@ function showRestart() {
 let count = 4;
 
 function genRandomNunber() {
-    const num = Math.floor(Math.random() * 50) + 1;
-    return num;
+    return Math.floor(Math.random() * 50) + 1;
+
 }
 let randomNumber = genRandomNunber();
 
@@ -56,21 +56,24 @@ onEvent('click', btn, function () {
     let maxNumber = 50;
     console.log(randomNumber);
     if (isNumber(userNumber)) {
+        --count;
 
-        if (count < 1) {
+        if (userNumber == randomNumber) {
+            output.innerText = `Congratulations You won`;
+            showRestart();
+            count = 4;
+            userInput.value = '';
+        } else if (userNumber > maxNumber || userNumber < 1) {
+            output.innerText = `Please enter number between 1 - 50`;
+            count = 4;
+        }
+        else if (count < 1) {
+
             output.innerText = `Your are run out of Chances: My number was ${randomNumber} `;
             gameOver.innerText = 'Please restart the game';
             showRestart();
-            output.innerText = ` ${result}`;
+            btn.disabled = true;
             count = 4;
-        } else if (userNumber > maxNumber || userNumber < 1) {
-            output.innerText = `Please enter number between 1 - 50`;
-            count = 5;
-        }
-        else if (userNumber == randomNumber) {
-            output.innerText = `Congratulations You won:`;
-            showRestart();
-            userInput.value = '';
 
 
         } else if (userNumber < randomNumber) {
@@ -79,7 +82,6 @@ onEvent('click', btn, function () {
             output.innerText = `My number is smaller : You have  ${count} chances `;
 
         }
-        count--;
     } else {
         output.innerText = 'Please enter numbers only between 1 - 50';
     }
@@ -89,10 +91,5 @@ onEvent('click', btn, function () {
 
 
 onEvent('click', restart, function () {
-    output.innerText = '';
-    gameOver.innerText = '';
-    userInput.value = '';
-    count = 4;
-    restart.classList.remove('restart-show');
-    genRandomNumber();
+    window.location.reload();
 });
